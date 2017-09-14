cf7525a092cea3a6922d8baff491bdc332e4f814
kevinwu <kevin@yichenk.com>
2017-09-14 18:17:48 +0800

    更新  blog列表的样式

--- blog.yichenk.com/web/src/script/page/home.vue
+++ blog.yichenk.com/web/src/script/page/home.vue
@@ -1,10 +1,9 @@
 <template>
     <div id="home">
-        <demo1></demo1>
         <div v-for="blog in blogList" class="blogList">
-            <h3 v-text="'title:'+blog.title"></h3>
-            <p v-text="'作者:'+blog.author"></p>
-            <p v-text="'标签:'+blog.label"></p>
+            <h2 v-text="blog.title"></h2>
+            <p v-text="'作者 : '+blog.author" class="author"></p>
+            <p v-text="'标签 : '+blog.label" class="label"></p>
             <div class="content" v-html="blog.body"></div>
         </div>
     </div>
@@ -16,7 +15,8 @@
 		name: 'home',
         data:function(){
 			return {
-				blogList:[]
+				blogList:[],
+                labels:[]
             }
         },
 		components: {
@@ -30,7 +30,6 @@
 				axios.get('/blog/findAll').then(function(data){
 					if(data.status === 200){
 						self.blogList = self.blogList.concat(data.data);
-                        console.log(self.blogList);
                     }
 				})
             },
@@ -44,5 +43,35 @@
 <style lang="less" scoped>
     #home{
         margin-top: 30px;
+        .blogList{
+            background: #fff;
+            padding: 10px;
+            margin-top: 25px;
+            box-shadow: 0 0 15px 1px #d1d1d1;
+            h2{
+                text-align: center;
+                margin: 0;
+                font-size: 30px;
+            }
+            p{
+                margin: 0;
+            }
+            .author{
+                text-align: center;
+                padding: 5px 0;
+                font-size: 13px;
+                color: #7c7c7c;
+            }
+            .label{
+                text-align: center;
+                padding: 10px;
+                font-size: 12px;
+                color: #9e9e9e;
+            }
+            .content{
+                padding: 10px;
+                border: 1px solid #e2e2e2;
+            }
+        }
     }
 </style>
