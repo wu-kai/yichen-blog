75baddf41c8dbb26046e1c93980b9955f0f2c5ec
kevinwu <649308287@qq.com>
2018-07-05 15:51:16 +0800

    微信分享

--- yichenk-server/blog.app.js
+++ yichenk-server/blog.app.js
@@ -2,10 +2,25 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./common/db');
-var demoRouter = require('./api/demo.router');
-var blogRouter = require('./api/blog.router');
-var ueditor = require('ueditor');
+const demoRouter = require('./api/demo.router');
+const blogRouter = require('./api/blog.router');
+const ueditor = require('ueditor');
+const wx = require('wechat-jssdk');
+const request = require('request');
+const qs=require('qs');
+const sha1 = require('sha1');
+const waterfall = require('async/waterfall');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({stdTTL: 3600, checkperiod: 3600}); //3600秒后过过期
 
+
+
+const TOKEN = 'qwesadsftrheytrufghdfsbhmyte';
+const wechatConfig = {
+	wechatToken: TOKEN,
+	appId: "wx12266588c2a4fbe3",
+	appSecret: "1b8e29a105a9934a77a32aeb9ecc39fd",
+};
 const app = express();
 
 const BODY_PARSER_MAX_BYTES = 1024 * 1024 * 10; // 10MB allowed to receive body content
@@ -54,6 +69,197 @@ app.use('/build',express.static(path.normalize(__dirname+'/../build')));
 app.use('/libStatic',express.static(path.normalize(__dirname+'/lib')));
 app.use('/ueditor',express.static(path.normalize(__dirname+'/images/ueditor')));
 app.use('/static',express.static(path.normalize(__dirname+'/dist/static')));
+
+function checkSignature(params,token){
+	var key=[token,params.timestamp,params.nonce].sort().join('');
+	//将token （自己设置的） 、timestamp（时间戳）、nonce（随机数）三个参数进行字典排序
+	var sha1=require('crypto').createHash('sha1');
+	//将上面三个字符串拼接成一个字符串再进行sha1加密
+	sha1.update(key);
+	return sha1.digest('hex') ==params.signature;
+	//将加密后的字符串与signature进行对比，若成功，返回echostr
+}
+
+// app.get('/wxJssdk/getJssdk', (req, res) => {
+//
+// 	const grant_type = 'client_credential';
+// 	const appid = 'wx12266588c2a4fbe3';
+// 	const secret = '1b8e29a105a9934a77a32aeb9ecc39fd';
+//
+// 	request('https://api.weixin.qq.com/cgi-bin/token?grant_type=' + grant_type + '&appid=' + appid + '&secret=' + secret, (err, response, body) => {
+// 		let access_toekn = JSON.parse(body).access_token;
+//
+// 		request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (err, response, body) => {
+// 			let jsapi_ticket = JSON.parse(body).ticket;
+// 			let nonce_str = '123456';    // 密钥，字符串任意，可以随机生成
+// 			let timestamp = new Date().getTime();  // 时间戳
+// 			let url = req.query.url;   // 使用接口的url链接，不包含#后的内容
+//
+// 			// 将请求以上字符串，先按字典排序，再以'&'拼接，如下：其中j > n > t > u，此处直接手动排序
+// 			let str = 'jsapi_ticket=' + jsapi_ticket + '&noncestr=' + nonce_str + '&timestamp=' + timestamp + '&url=' + url;
+//
+// 			// 用sha1加密
+// 			let signature = sha1(str);
+//
+// 			res.send({
+// 				appId: appid,
+// 				timestamp: timpstamp,
+// 				nonceStr: nonce_str,
+// 				signature: signature,
+// 			})
+// 		})
+// 	})
+// });
+
+
+app.get('/wxJssdk/getJssdk', (req, res) => {
+	let grant_type = 'client_credential';
+	let appid = 'wx12266588c2a4fbe3';
+	let secret = '1b8e29a105a9934a77a32aeb9ecc39fd'; // appscret
+
+	let steps = [];
+
+	// 第一步，获取access_token
+	steps.push((cb) => {
+
+		let steps1 = []
+
+		// 第1.1步，从缓存中读取access_token
+		steps1.push((cb1) => {
+			let access_token = cache.get('access_token', (err, access_token) => {
+				cb1(err, access_token)
+			})
+		})
+
+		// 第1.2步，缓存中有access_token则直接返回，如果没有，则从服务器中读取access_token
+		steps1.push((access_token, cb1) => {
+			if (access_token) {
+				cb1(null, access_token, 'from_cache')
+			} else {
+				request('https://api.weixin.qq.com/cgi-bin/token?grant_type=' + grant_type + '&appid=' + appid + '&secret=' + secret, (err, response, body) => {
+					cb1(err, JSON.parse(body).access_token, 'from_server')
+				})
+			}
+		})
+
+		// 第1.3步，如果是新从服务器取的access_token，则缓存起来，否则直接返回
+		steps1.push((access_token, from_where, cb1) => {
+			if (from_where === 'from_cache') {
+				console.log(' === 成功从缓存中读取access_token: ' + access_token + ' ===')
+				cb1(null, access_token)
+			} else if (from_where === 'from_server') {
+				cache.set('access_token', access_token, (err, success) => {
+					if (!err && success) {
+						console.log(' === 缓存已过期，从服务器中读取access_token: ' + access_token + ' ===')
+						cb1(null, access_token)
+					} else {
+						cb1(err || 'cache设置access_token时，出现未知错误')
+					}
+				})
+			} else {
+				cb1('1.3获取from_where时，from_where值为空')
+			}
+		})
+
+
+
+		waterfall(steps1, (err, access_token) => {
+			cb(err, access_token)
+		})
+	});
+
+
+	// 第二步，获取ticket
+	steps.push((access_token, cb) => {
+		let steps1 = []
+
+		// 第2.1步，从缓存中读取ticket
+		steps1.push((cb1) => {
+			let ticket = cache.get('ticket', (err, ticket) => {
+				cb1(err, ticket)
+			})
+		})
+
+		// 第2.2步，缓存中有ticket则直接返回，如果没有，则从服务器中读取ticket
+		steps1.push((ticket, cb1) => {
+			if (ticket) {
+				cb1(null, ticket, 'from_cache')
+			} else {
+				request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (err, response, body) => {
+					cb1(err, JSON.parse(body).ticket, 'from_server')
+				})
+			}
+		})
+
+		// 第2.3步，如果新从服务器取的ticket，则缓存起来，否则直接返回
+		steps1.push((ticket, from_where, cb1) => {
+			if (from_where === 'from_cache') {
+				console.log(' === 成功从缓存中读取ticket: ' + ticket + ' ===')
+				cb1(null, ticket)
+			} else if (from_where === 'from_server') {
+				cache.set('ticket', ticket, (err, success) => {
+					if (!err && success) {
+						console.log(' === 缓存已过期，从服务器中读取ticket: ' + ticket + ' ===');
+						cb1(null, ticket)
+					} else {
+						cb1(err || 'cache设置ticket时，出现未知错误')
+					}
+				})
+			} else {
+				cb1('2.3获取from_where时，from_where值为空')
+			}
+		})
+
+		waterfall(steps1, (err, ticket) => {
+			cb(err, ticket)
+		})
+	});
+
+
+	// 第三步，生成签名
+	steps.push((ticket, cb) => {
+		let jsapi_ticket = ticket
+		let nonce_str = '123456'
+		let timestamp = new Date().getTime()
+		let url = req.query.url
+
+		let str = 'jsapi_ticket=' + jsapi_ticket + '&noncestr=' + nonce_str + '&timestamp=' + timestamp + '&url=' + url
+		let signature = sha1(str)
+
+		cb(null, {
+			appId: appid,
+			timestamp: timestamp,
+			nonceStr: nonce_str,
+			signature: signature,
+			ticket: ticket
+		})
+	});
+
+	waterfall(steps, (err, data) => {
+		if (err) {
+			res.send({status: 'error', data: err})
+		} else {
+			res.send({status: 'success', data: data})
+		}
+	})
+});
+
+app.get('/getToken',function(req,res){
+	var query=require('url').parse(req.url).query;
+	var params=qs.parse(query);
+
+	console.log(params);
+
+	if(!checkSignature(params,TOKEN)){
+		//如果签名不对，结束请求并返回
+		res.end('signature fail');
+	}else{
+		if (req.method == "GET") {
+			//如果请求是GET，返回echostr用于通过服务器有效校验
+			res.end(params.echostr);
+		}
+	}
+});
 
 app.all('/',function(req,res){
 	if(isPro){
--- yichenk-web/src/main.js
+++ yichenk-web/src/main.js
@@ -15,10 +15,58 @@ import util from './util'
 require('./css/font-awesome.css');
 require('./css/common.css');
 
+
 Vue.use(VueBlu);
 
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios;
+
+const wxShare = {
+  title:'test',
+  desc:'only test',
+  link:'yichenk.com',
+  imgUrl:'http://p9kmzrcfb.bkt.clouddn.com/default-blog-img-1.jpg'
+};
+if (wx) {
+  axios.get('/wxJssdk/getJssdk', {url: location.href}).then((response) => {
+    var data = response.data;
+
+    console.log(data);
+
+    wx.config({
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: data.appId, // 必填，公众号的唯一标识
+      timestamp: data.timestamp, // 必填，生成签名的时间戳
+      nonceStr: data.nonceStr, // 必填，生成签名的随机串
+      signature: data.signature,// 必填，签名，见附录1
+      jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    });
+
+    wx.ready(function () {
+
+      console.log('ready '+ wxShare.title);
+
+      wx.onMenuShareTimeline({
+        title: wxShare.title,
+        desc: wxShare.desc,
+        link: wxShare.link,
+        imgUrl: wxShare.imgUrl
+      });
+
+      wx.onMenuShareAppMessage({
+        title: wxShare.title,
+        desc: wxShare.desc,
+        link: wxShare.link,
+        imgUrl: wxShare.imgUrl
+      });
+    });
+
+    wx.error(function (res) {
+      console.log(res.errMsg);
+      // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+    })
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
