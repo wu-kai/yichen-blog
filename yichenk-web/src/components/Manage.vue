916d9c9fc68a144586ebdf8473d0560ac2efa0a7
kevinwu <649308287@qq.com>
2018-07-12 11:50:35 +0800

    更新

--- yichenk-web/src/components/Manage.vue
+++ yichenk-web/src/components/Manage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="manage-box">
     <div class="menu">
-      <router-link class="menu-item" to="/manage">首页</router-link>
-      <router-link class="menu-item" to="/manage/createBlog">写文章</router-link>
+      <router-link class="menu-item"
+                   to="/manage"
+                   :class="[path === '/manage'?'active':'']">首页</router-link>
+      <router-link class="menu-item"
+                   to="/manage/createBlog"
+                   :class="[path === '/manage/createBlog'?'active':'']">写文章</router-link>
     </div>
     <div class="content">
       <router-view></router-view>
@@ -11,25 +15,37 @@
 </template>
 <script>
   export default {
-
+    data:function(){
+      return {
+        path:''
+      }
+    },
+    created(){
+      this.path = this.$route.path;
+      this.$watch('$route',function(n){
+        this.path = this.$route.path;
+      });
+    }
   };
 </script>
 <style scoped>
-  .manage-box{
+  .manage-box {
     overflow: hidden;
     height: 100%;
   }
-  .menu{
+
+  .menu {
     width: 160px;
     float: left;
     overflow: hidden;
     height: 100%;
     box-sizing: border-box;
     text-align: left;
-    box-shadow: 0 1px 4px rgba(0,0,0,.15);
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .15);
     padding-top: 70px;
   }
-  .menu-item{
+
+  .menu-item {
     float: left;
     width: 100%;
     box-sizing: border-box;
@@ -37,15 +53,23 @@
     line-height: 30px;
     border-top: 1px solid #c8c8c8;
   }
-  .menu-item:last-child{
+
+  .menu-item.active{
+    background: #d9d9d9;
+    color: #fff;
+  }
+
+  .menu-item:last-child {
     border-bottom: 1px solid #c8c8c8;
   }
-  .menu-item:hover{
+
+  .menu-item:hover {
     cursor: pointer;
     background: #d9d9d9;
     color: #fff;
   }
-  .content{
+
+  .content {
     width: calc(100% - 170px);
     float: left;
     height: 100%;
