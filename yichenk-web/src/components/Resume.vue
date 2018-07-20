1ebc964bfba1c2429deaa4fd5d5789a246c33ff8
kevinwu <649308287@qq.com>
2018-07-20 11:57:02 +0800

    更新简历

--- yichenk-web/src/components/Resume.vue
+++ yichenk-web/src/components/Resume.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="resume">
-        <h1>简历</h1>
-        <p>还没做</p>
-        <p>做了也不会开放</p>
-        <p>等着吧</p>
+      <button onclick="location.href = 'http://www.yichenk.com/libStatic/resume.html'">在线浏览</button>
+      <button onclick="location.href = 'http://pc5833j8t.bkt.clouddn.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.doc'">下载PDF</button>
+      <button onclick="location.href = 'http://pc5833j8t.bkt.clouddn.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.pdf'">下载DOC</button>
     </div>
 </template>
 
@@ -15,6 +14,11 @@
 
 <style scoped>
     .resume{
-        margin-top:100px;
+        padding-top:300px;
+    }
+
+    button:hover{
+      color: #00ff00;
+      background: #0f0f0f;
     }
 </style>
