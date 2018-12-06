82d47a070b99c4d13c2f6c76782039811cf305d7
kevinwu <649308287@qq.com>
2018-12-06 11:45:47 +0800

    edit resume

--- yichenk-web/src/components/Resume.vue
+++ yichenk-web/src/components/Resume.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="resume">
       <button onclick="location.href = 'http://www.yichenk.com/libStatic/resume.html'">在线浏览</button>
-      <button onclick="location.href = 'http://pc5833j8t.bkt.clouddn.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.doc'">下载PDF</button>
-      <button onclick="location.href = 'http://pc5833j8t.bkt.clouddn.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.pdf'">下载DOC</button>
+      <button onclick="location.href = 'http://cdn.yichenk.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.pdf'">下载PDF</button>
+      <button onclick="location.href = 'http://cdn.yichenk.com/%E5%90%B4%E5%87%AF%E7%9A%84%E7%AE%80%E5%8E%86.doc'">下载DOC</button>
     </div>
 </template>
 
