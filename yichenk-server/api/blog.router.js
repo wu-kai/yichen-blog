ee17b6607ec61afcb40b62de920517a9803fbb73
wu.kai <wu.kai@navercorp.com>
2021-01-28 17:50:53 +0800

    save

--- yichenk-server/api/blog.router.js
+++ yichenk-server/api/blog.router.js
@@ -1,75 +1,71 @@
+const express = require('express');
+const router = express.Router();
 
-var express = require('express');
-var router = express.Router();
+const { blog } = require('./instance');
 
-var blog = require('./blog.model').blog;
-
-router.all('/',function(req,res){
-    res.jsonp({message:'hello'});
+router.all('/', function (req, res) {
+  res.jsonp({ message: 'hello' });
 });
 
-router.post('/createBlog',function(req,res){
-    var data = req.body;
-    blog.web.create(data,function(err,ok){
-        if(err){
-            res.jsonp(err);
-        }else{
-            res.jsonp({status:'success',data:ok})
-        }
-    });
+router.post('/createBlog', function (req, res) {
+  const data = req.body;
+  blog.create(data, function (err, ok) {
+    if (err) {
+      res.jsonp(err);
+    } else {
+      res.jsonp({ status: 'success', data: ok })
+    }
+  });
 });
 
-router.get('/findAll',function(req,res){
-    blog.findAll(function(err,ok){
-        if(err){
-            res.jsonp(err);
-        }else{
-            res.jsonp(ok);
-        }
-    })
+router.get('/findAll', function (req, res) {
+  blog.findAll(function (err, ok) {
+    if (err) {
+      res.jsonp(err);
+    } else {
+      res.jsonp(ok);
+    }
+  })
 });
 
-router.get('/findByID',function(req,res){
-	var id=req.query.id;
-	blog.web.findById(id,function(err,ok){
-		if(err){
-			res.jsonp(err);
-		}else{
-			res.jsonp(ok);
-		}
-	});
+router.get('/findByID', function (req, res) {
+  const id = req.query.id;
+  blog.findById(id, function (err, ok) {
+    if (err) {
+      res.jsonp(err);
+    } else {
+      res.jsonp(ok);
+    }
+  });
 });
 
-router.post('/deleteBlogById',function(req,res){
-	var id=req.body.id;
-	blog.web.deleteById(id,function(err,ok){
-		if(err){
-			res.jsonp(err);
-		}else{
-			res.jsonp(ok);
-		}
-	});
+router.post('/deleteBlogById', function (req, res) {
+  const id = req.body.id;
+  blog.deleteById(id, function (err, ok) {
+    if (err) {
+      res.jsonp(err);
+    } else {
+      res.jsonp(ok);
+    }
+  });
 });
-router.post('/editBlog',function(req,res){
-	var data = req.body;
-	blog.web.editBlog(data,function(err,ok){
-		if(err){
-			res.jsonp(err);
-		}else{
-			res.jsonp(ok);
-		}
-	});
+router.post('/editBlog', function (req, res) {
+  const data = req.body;
+  blog.editBlog(data, function (err, ok) {
+    if (err) {
+      res.jsonp(err);
+    } else {
+      res.jsonp(ok);
+    }
+  });
 });
-router.get('/validate',function(req,res){
-	var value = req.query.validateValue;
-	if(value === 'manage'){
-		res.jsonp({result:true})
-	}else{
-		res.jsonp({result:false})
-	}
+router.get('/validate', function (req, res) {
+  const value = req.query.validateValue;
+  if (value === 'manage') {
+    res.jsonp({ result: true })
+  } else {
+    res.jsonp({ result: false })
+  }
 });
-
-
 
 module.exports = router;
-
--- yichenk-server/api/comment.router.js
+++ yichenk-server/api/comment.router.js
@@ -2,59 +2,59 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-var comment = require('./comment.model').comment;
+const { comment } = require('./instance');
 
 function getClientIp(req) {
-	return req.headers['x-forwarded-for'] ||
-		req.connection.remoteAddress ||
-		req.socket.remoteAddress ||
-		req.connection.socket.remoteAddress;
+  return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
 };
 
-router.post('/createComment',function(req,res){
-	let data = req.body;
-	let ipStr = getClientIp(req);
-	let arr = ipStr.split(':');
-	let ip = arr[arr.length-1];
-	if(ip === 1 || ip === '1' || ip === '127.0.0.1'){
-		data.city = '天上';
-		comment.create(data,function(err,result){
-			if(err){
-				res.json(err);
-			}else{
-				res.json({status:'success',data:result})
-			}
-		})
-	}else{
-		let apiPath = 'http://api.map.baidu.com/location/ip?ip='+ip+'&ak=Q8TuGc4hjSnH9hBKUzZGS8qtWqM4Simh&coor=bd09ll';
-		request(apiPath,(err, response, body) => {
-			let content = JSON.parse(body).content.address_detail;
-			let province = content.province;
-			let city = content.city;
-			if(city.substr(city.length-1,1) === '市'){
-				city = city.substr(0,city.length-1)
-			}
-			data.city = city;
-			data.province = province;
-			comment.create(data,function(err,result){
-				if(err){
-					res.json(err);
-				}else{
-					res.json({status:'success',data:result})
-				}
-			})
-		});
-	}
+router.post('/createComment', function (req, res) {
+  let data = req.body;
+  let ipStr = getClientIp(req);
+  let arr = ipStr.split(':');
+  let ip = arr[arr.length - 1];
+  if (ip === 1 || ip === '1' || ip === '127.0.0.1') {
+    data.city = '天上';
+    comment.create(data, function (err, result) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json({ status: 'success', data: result })
+      }
+    })
+  } else {
+    let apiPath = 'http://api.map.baidu.com/location/ip?ip=' + ip + '&ak=Q8TuGc4hjSnH9hBKUzZGS8qtWqM4Simh&coor=bd09ll';
+    request(apiPath, (err, response, body) => {
+      let content = JSON.parse(body).content.address_detail;
+      let province = content.province;
+      let city = content.city;
+      if (city.substr(city.length - 1, 1) === '市') {
+        city = city.substr(0, city.length - 1)
+      }
+      data.city = city;
+      data.province = province;
+      comment.create(data, function (err, result) {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json({ status: 'success', data: result })
+        }
+      })
+    });
+  }
 });
 
-router.get('/findAll',function(req,res){
-	comment.findAll(function(err,result){
-		if(err){
-			res.json(err);
-		}else{
-			res.json({status:'success',data:result});
-		}
-	})
+router.get('/findAll', function (req, res) {
+  comment.findAll(function (err, result) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json({ status: 'success', data: result });
+    }
+  })
 });
 
 module.exports = router;
